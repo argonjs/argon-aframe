60b84958e0729ae9ef8e035daeff8a158436184b
Blair MacIntyre <bmacintyre@mozilla.com>
2017-05-02 16:28:35 -0400

    adding dist

--- dist/argon-aframe.js
+++ dist/argon-aframe.js
@@ -142,6 +142,9 @@
 
 	        this.argonRender = this.argonRender.bind(this);
 	        this.argonUpdate = this.argonUpdate.bind(this);
+	        this.argonPresentChange = this.argonPresentChange.bind(this);
+	        this.argonChangeReality = this.argonChangeReality.bind(this);
+
 	        this.initializeArgonView = this.initializeArgonView.bind(this);
 
 	        this.addEventListener('render-target-loaded', function () {
@@ -219,14 +222,82 @@
 	        value: function () {
 	            this.argonApp.renderEvent.addEventListener(this.argonRender);
 	            this.argonApp.updateEvent.addEventListener(this.argonUpdate);
+
+	            this.argonApp.device.presentHMDChangeEvent.addEventListener(this.argonPresentChange);
+	            this.argonApp.reality.changeEvent.addEventListener(this.argonChangeReality);
 	        },
 	        writable: true
+	    },
+
+	    argonChangeReality: {
+	      value: function () {
+	        // for now, we just revisit the presentation setup
+	        this.argonPresentChange();
+	      },
+	      writable: true
+	    },
+
+	    argonPresentChange: {
+	      value: function () {
+	        var device = this.argonApp.device;
+	        var reality = this.argonApp.reality;
+	        var visible = false;
+
+	        // AFrame already uses "vr-mode" to mean "isPresenting()" in WebVR, which means either
+	        // presenting on an HMD in WebVR, or on mobile w/ cardboard and the WebVR Polyfill
+	        //
+	        // While this isn't exactly what we want, we'll assume that this means "presenting in an HMD"
+	        // We'll add "AR" to signify that there is a version of Reality showing behind the content.
+	        // Again, while not precisely correct, it is ok. 
+	        if (device.isPresentingHMD) {
+	          if (!this.is('vr-mode')) {
+	            this.addState('vr-mode');
+	            this.emit('enter-vr', {target: this});
+	          }
+
+	          // if we're in HMD mode, we determine AR mode from isPresentingRealityHMD
+	          if (device.isPresentingRealityHMD) {
+	            if (!this.is('ar-mode')) {
+	              this.addState('ar-mode');
+	              this.emit('enter-ar', {target: this});
+	            }
+	          } else {
+	            if (this.is('ar-mode')) {
+	              this.removeState('ar-mode');
+	              this.emit('exit-ar', {target: this});
+	            }
+	          }
+	        } else {
+	          if (this.is('vr-mode')) {
+	            this.removeState('vr-mode');
+	            this.emit('exit-vr', {target: this});
+	          }
+
+	          // if we're not in HMD mode, we determine AR mode based on the current reality.
+	          // the "empty" reality is not considered AR, which is the default reality on a 
+	          // browser than isn't see-through or can't display live video
+	          if (reality.current != Argon.RealityViewer.EMPTY) {
+	            if (!this.is('ar-mode')) {
+	              this.addState('ar-mode');
+	              this.emit('enter-ar', {target: this});
+	            }
+	          } else {
+	            if (this.is('ar-mode')) {
+	              this.removeState('ar-mode');
+	              this.emit('exit-ar', {target: this});
+	            }
+	          }
+	        }
+	      },
+	      writable: true
 	    },
 
 	    removeEventListeners: {
 	        value: function () {
 	            this.argonApp.updateEvent.removeEventListener(this.argonUpdate);
 	            this.argonApp.renderEvent.removeEventListener(this.argonRender);
+	            this.argonApp.device.presentChangeEvent.removeEventListener(this.argonPresentChange);
+	            this.argonApp.reality.changeEvent.removeEventListener(this.argonChangeReality);
 	        },
 	        writable: true
 	    },
@@ -242,6 +313,9 @@
 
 	        this.addEventListener('loaded', function () {
 	          if (this.renderStarted) { return; }
+
+	          // only do this once!
+	          this.renderStarted = true;
 
 	          var fixCamera = function () {
 	            var arCameraEl = null;
@@ -296,7 +370,6 @@
 	              window.performance.mark('render-started');
 	          }
 
-	          this.renderStarted = true;
 	          this.emit('renderstart');
 	        });
 
@@ -478,17 +551,17 @@
 	        // unclear right now how much of the components that use vr-mode are re-purposable
 	        //var _a = app.view.getSubviews();
 	        var _a = app.view.subviews;
-	        if (this.is('vr-mode')) {
-	          if (_a.length == 1 && this.is('vr-mode')) {
-	            this.removeState('vr-mode');
-	            this.emit('exit-vr', {target: this});
-	          } 
-	        } else {
-	          if (_a.length > 1 && !this.is('vr-mode')) {
-	            this.addState('vr-mode');
-	            this.emit('enter-vr', {target: this});
-	          }
-	        }
+	        // if (this.is('vr-mode')) {
+	        //   if (_a.length == 1 && this.is('vr-mode')) {
+	        //     this.removeState('vr-mode');
+	        //     this.emit('exit-vr', {target: this});
+	        //   } 
+	        // } else {
+	        //   if (_a.length > 1 && !this.is('vr-mode')) {
+	        //     this.addState('vr-mode');
+	        //     this.emit('enter-vr', {target: this});
+	        //   }
+	        // }
 
 	        // set the camera properties to the values of the 1st subview.
 	        // While this is arbitrary, it's likely many of these will be the same
@@ -1636,15 +1709,18 @@
 	    var thisPos = object3D.getWorldPosition();
 	    var distance = thisPos.distanceTo(cameraPos);
 
-	    // base the factor on the viewport height
-	    var viewport = this.el.sceneEl.argonApp.view.viewport;
-	    this.factor = 2 * (this.scale / viewport.height); 
+	    // base the factor on the viewport height.   
+	    // I think we need to use the renderviewport size, since we really care about what is rendered.
+	    // This means that when we're rendering on an HMD, and are using this to scale HTML content, the content might be 
+	    // the wrong size
+	    var height = this.el.sceneEl.argonApp.view.renderHeight;
+	    this.factor = 2 * (this.scale / height); 
 
 	    // let's get the fov scale factor from the camera
 	    fovScale = Math.tan(THREE.Math.degToRad(camera.fov) / 2) * 2;
 
-	    // if distance < near clipping plane, just use scale.  Don't go any bigger
-	    var factor = fovScale * (distance < camera.near ? this.factor : distance * this.factor);
+	    // if distance < near clipping plane, just use scale at the near plane.  Don't go any bigger
+	    var factor = fovScale * (distance < camera.near ? camera.near * this.factor : distance * this.factor);
 	    object3D.scale.set(factor, factor, factor);
 	  }
 	});
@@ -1666,6 +1742,66 @@
 	    if (this.data && evt.detail.target === this.el) {
 	      this.el.object3D.visible = evt.detail.found;
 	    }
+	  },
+
+	  update: function () {
+	  }
+	}); 
+
+	AFRAME.registerComponent('physical', {
+	  schema: { 
+	    default: true
+	  },
+
+	  init: function () {
+	  },
+
+	  // have to use tick and do this every frame since "mesh" could change and 
+	  // we won't be notified.  Bummer
+	  tick: function () {
+	    if (this.data) {
+	      var mesh = this.el.getObject("mesh");
+	      if (mesh) {
+	      	mesh.material.colorWrite = false; // only update the depth
+		      mesh.renderOrder = -1;   // before everything else
+	      }  
+	    }
+	  }
+	}); 
+
+	AFRAME.registerComponent('show-in', {
+	  schema: { 
+	    ar: {default: false},
+	    vr: {default: false},
+	    "arhmd": { default: false},
+	    "vrhmd": { default: false} 
+	  },
+
+	  init: function () {
+	    var self = this;
+	    this.el.sceneEl.addEventListener('enter-vr', function (evt) { self.updateVisibility(evt); });
+	    this.el.sceneEl.addEventListener('exit-vr', function (evt) { self.updateVisibility(evt); });
+	    this.el.sceneEl.addEventListener('enter-ar', function (evt) { self.updateVisibility(evt); });
+	    this.el.sceneEl.addEventListener('exit-ar', function (evt) { self.updateVisibility(evt); });
+	  },
+
+	  updateVisibility: function (evt) {
+	    if (evt.detail.target != this.el) {
+	      return;
+	    }
+
+	    var armode = this.is('ar-mode');
+	    var hmdmode = this.is('vr-mode');
+
+	    var data = this.data;
+	    var visible = false;
+
+	    if (data.arhmd && armode && hmdmode) { visible = true;}
+	    if (data.vrhmd && !armode && hmdmode) { visible = true;}
+	    if (data.ar && armode) { visible = true;}
+	    if (data.vr && !armode) { visible = true;}
+	  
+	    this.el.object3D.visible = visible;
 	  },
 
 	  update: function () {
